028a71239da9787c4a11740ecfae7000a8411395
dhaya1407 <dhayanithi1407@gmail.com>
2023-09-03 15:04:04 +0530

    new

---
+++ src/org/dvf/Ddd.java
@@ -0,0 +1,41 @@
+package org.dvf;
+
+public class Ddd {
+
+	
+	private void m1() {
+
+		System.out.println("dhaya");
+
+	}
+	private void m2() {
+
+		System.out.println("ashok");
+
+	}
+	private void m3() {
+
+		System.out.println("murali");
+	
+	}
+	private void m4() {
+
+		System.out.println("karthik");
+	
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Ddd c = new Ddd();
+		c.m1();
+		c.m2();
+		c.m3();
+		c.m4();
+		
+
+		
+	}
+}
